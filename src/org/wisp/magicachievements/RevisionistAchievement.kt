655671aa10d2a22dbc6d43abf092bfdafdfb7418
Wisp <1343347+wispborne@users.noreply.github.com>
2024-03-01 21:35:15 -0800

    Fixed revisionist.

--- src/org/wisp/magicachievements/RevisionistAchievement.kt
+++ src/org/wisp/magicachievements/RevisionistAchievement.kt
@@ -8,6 +8,9 @@ import org.magiclib.achievements.MagicAchievement
 import org.magiclib.paintjobs.MagicPaintjobManager
 import java.util.*
 
+/**
+ * This achievement is unlocked when the player reloads a save and fights the same battle again.
+ */
 class RevisionistAchievement : MagicAchievement() {
     companion object {
         private const val FLEET_IDS_HASHED_KEY = "magicachievement_revisionist_fleetIdsInBattleHash"
@@ -35,13 +38,13 @@ class RevisionistAchievement : MagicAchievement() {
         if (interval.intervalElapsed()) {
             // This line is why I'm using Kotlin instead of Java.
             val allFleetIdsInBattle =
-                Global.getCombatEngine().ships.mapNotNull { it.fleetMember?.fleetData?.fleet?.id }.distinct()
+                Global.getCombatEngine().ships.mapNotNull { it.fleetMember?.captain?.fleet?.id }.distinct()
             val fleetIdsHashed = Objects.hash(allFleetIdsInBattle)
             if (allFleetIdsInBattle.size < 2) return // During deployment, the other fleet is not in the battle yet.
 
             // If the fleet ids hash is the same as what was stored, then the player is either
             // in the same battle as last `advanceInCombat`, or in a reloaded battle with the same fleets.
-            if (memory[FLEET_IDS_HASHED_KEY] == fleetIdsHashed) {
+            if (memory[FLEET_IDS_HASHED_KEY] as? Int == fleetIdsHashed) {
                 // If the "loaded save" is true, then they are fighting the same fleets after (re)loading a save.
                 if (memory[LOADED_SAVE_KEY] == true) {
                     completeAchievement()
